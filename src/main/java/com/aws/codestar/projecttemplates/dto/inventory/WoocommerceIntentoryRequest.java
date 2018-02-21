24a3a21a5372da6778be2caebaf5aa4567d56db8
isoto <israel.soto@muuyal.mx>
2018-02-21 16:06:19 -0700

    packages creation

---
+++ src/main/java/com/aws/codestar/projecttemplates/dto/inventory/WoocommerceIntentoryRequest.java
@@ -0,0 +1,7 @@
+package com.aws.codestar.projecttemplates.dto.inventory;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WoocommerceIntentoryRequest {
+}
